8c2c4d24e2e89a6615be3c70dba691fa6b143d0d
Jesper941 <>
2023-10-18 03:27:49 +0000

    Import os and random. Add grid dimensions and ship sizes. Create grid.

--- run.py
+++ run.py
@@ -1,3 +1,16 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import os
+import random
+
+# Constants for grid dimensions
+ROWS = 10
+COLS = 10
+
+# Constants for ship sizes
+SHIP_SIZES = {'Carrier': 5, 'Battleship': 4, 'Cruiser': 3, 'Submarine': 3, 'Destroyer': 2}
+
+# Create an empty grid and a grid to track hits, initially marking ship locations as '~'
+grid = [['~' for _ in range(COLS)] for _ in range(ROWS)]
+hits_grid = [['~' for _ in range(COLS)] for _ in range(ROWS)]
+
+# Create a dictionary to store ship data
+ships = {}
